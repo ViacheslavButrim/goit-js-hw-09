60b3b98770f18e2a3afda96bf5a0d754ff3c9089
Vik <viacheslavbutrim@gmail.com>
2025-10-10 01:18:33 +0300

    Update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,39 +1,51 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.querySelector('.feedback-form');
-  const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
 
-  let formData = { email: '', message: '' };
+// object with initial values
+let formData = {
+  email: '',
+  message: '',
+};
 
-  const savedData = localStorage.getItem(STORAGE_KEY);
-  if (savedData) {
-    formData = JSON.parse(savedData);
-    form.elements.email.value = formData.email || '';
-    form.elements.message.value = formData.message || '';
+// data recovery from localStorage 
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+  formData = JSON.parse(savedData);
+  form.elements.email.value = formData.email || '';
+  form.elements.message.value = formData.message || '';
+}
+
+// listening to event input on a form
+form.addEventListener('input', event => {
+  const { name, value } = event.target;
+  if (!name) return; // skip if element without attribute name
+
+  // save only store truncated values
+  formData[name] = value.trim();
+
+  // save actual object in localStorage
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+// processing submit
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  // update formData before departure
+  formData.email = form.elements.email.value.trim();
+  formData.message = form.elements.message.value.trim();
+
+  // check filling both fields
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
   }
 
-  form.addEventListener('input', event => {
-    const { name, value } = event.target;
-    if (!name) return;
+  // conclusion actual object in console
+  console.log(formData);
 
-    formData[name] = value; // без trim
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-  });
-
-  form.addEventListener('submit', event => {
-    event.preventDefault();
-
-    formData.email = form.elements.email.value.trim();
-    formData.message = form.elements.message.value.trim();
-
-    if (!formData.email || !formData.message) {
-      alert('Fill please all fields');
-      return;
-    }
-
-    console.log(formData);
-
-    localStorage.removeItem(STORAGE_KEY);
-    form.reset();
-    formData = { email: '', message: '' };
-  });
+  // clear localStorage, object and form
+  //localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+  formData = { email: '', message: '' };
 });
