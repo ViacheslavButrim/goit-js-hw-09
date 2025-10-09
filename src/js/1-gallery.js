81e3fd440daec9f5de06713fc42828f4fd17bdb5
Vik <viacheslavbutrim@gmail.com>
2025-10-09 13:23:47 +0300

    Update 1-gallery.js

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -17,51 +17,51 @@ const images = [
   },
   {
     preview:
-      './public/dev-motto-min.jpg',
+      '/goit-js-hw-09/dev-motto-min.jpg',
     original:
-      './public/dev-motto@4x-min.jpg',
+      '/goit-js-hw-09/dev-motto@4x-min.jpg',
     description: 'A Motto On The Phone Screen',
   },
   {
     preview:
-      './public/mettroom-min.jpg',
+      '/goit-js-hw-09/mettroom-min.jpg',
     original:
-      './public/mettroom@4x-min.jpg',
+      '/goit-js-hw-09/mettroom@4x-min.jpg',
     description: 'Meeting Room',
   },
   {
     preview:
-      './public/chillroom-min.jpg',
+      '/goit-js-hw-09/chillroom-min.jpg',
     original:
-      './public/chillroom@4x-min.jpg',
+      '/goit-js-hw-09/chillroom@4x-min.jpg',
     description: 'Leisure Room',
   },
   {
     preview:
-      './public/office-min.jpg',
+      '/goit-js-hw-09/office-min.jpg',
     original:
-      './public/office@4x-min.jpg',
+      '/goit-js-hw-09/office@4x-min.jpg',
     description: 'IT Company Office',
   },
   {
     preview:
-      './public/workspace-min.jpg',
+      '/goit-js-hw-09/workspace-min.jpg',
     original:
-      './public/workspace@4x-min.jpg',
+      '/goit-js-hw-09/workspace@4x-min.jpg',
     description: 'Workspace',
   },
   {
     preview:
-      './public/work-min.jpg',
+      '/goit-js-hw-09/work-min.jpg',
     original:
-      './public/work@4x-min.jpg',
+      '/goit-js-hw-09/work@4x-min.jpg',
     description: 'Girl Working At The Computer',
   },
   {
     preview:
-      './public/meet-min.jpg',
+      '/goit-js-hw-09/meet-min.jpg',
     original:
-      './public/meet@4x-min.jpg',
+      '/goit-js-hw-09/meet@4x-min.jpg',
     description: 'Working Meeting',
   },
 ];
